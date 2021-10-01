41b15432e68832f00fc938bf5be22f93fab704d1
nanzcakep <nandanugraha107@gmail.com>
2021-10-01 23:22:29 +0700

    hello_world_nanzcakep.php

---
+++ scripts/hello_world_nanzcakep.php
@@ -0,0 +1,13 @@
+<?php
+
+// LANGUAGE: PHP
+// ENV: hello_world_nanzcakep.php
+// AUTHOR: Alice Chuang
+// GITHUB: https://github.com/nanzcakep
+
+function SayHello()
+{
+    echo "Hello World";
+}
+
+SayHello();
